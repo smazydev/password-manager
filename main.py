c256f24197c18c116b8833837e5d09f5cd0fd8d9
smazydev <alizafar1437@gmail.com>
2020-12-29 21:52:44 +0500

    added password generator functionality

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from tkinter import *
-
+from tkinter import messagebox
+from random import choice,randint,shuffle
 
 
 #---------------------------------- PASSWORD GENERATOR -------------------------- #
 
-def password_generator():
-    pass
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_entry.insert(0,password)
 
 
 #---------------------------------- SAVE PASSWORD TO FILE -------------------------- #
 def save_credentials_to_file():
+
     with open("data.txt",mode="a") as data_file:
         data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
         website_entry.delete(0,END)
@@ -45,7 +65,7 @@ password_entry = Entry(width=17)
 password_entry.grid(row=3,column=1)
 
 #Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password",command=generate_password)
 generate_password_button.grid(row=3,column=2)
 add_button = Button(text="Add",width=36,command=save_credentials_to_file)
 add_button.grid(row=4,column=1,columnspan=2)
